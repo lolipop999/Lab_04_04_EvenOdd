2d902ec25f7c8b18d0af6813c932c06da72d07e0
lolipop999 <106931888+lolipop999@users.noreply.github.com>
2022-06-05 19:34:52 -0400

    Add files via upload

---
+++ Lab_04_04_EvenOdd/src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        int numToCheck = 7;
+        int nummodulo;
+
+        nummodulo = numToCheck % 2;
+
+        if (nummodulo == 1) {
+            System.out.println("The number is odd");
+        }
+
+        else {
+            System.out.println("The number is even");
+        }
+
+    }
+}
